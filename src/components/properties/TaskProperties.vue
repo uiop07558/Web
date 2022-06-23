abcd72b5ec23b27b5de15558e64ac60e39ea127f
Пользователь <xDMK@yandex.ru>
2022-06-23 10:21:49 +0300

    11111

--- src/components/properties/TaskProperties.vue
+++ src/components/properties/TaskProperties.vue
@@ -1046,7 +1046,7 @@ export default {
       // const seconds = this.pad2(date.getUTCSeconds())
       // const dateCreate = year + '-' + month + '-' + day + 'T' + hours + ':' + minutes + ':' + seconds
       let msgtask = msg || this.taskMsg
-      console.log('msgtask', msgtask, msg, this.taskMsg)
+      msgtask = msgtask.trim()
       msgtask = msgtask.replaceAll('&', '&amp;')
       msgtask = msgtask.replaceAll('<', '&lt;')
       msgtask = msgtask.replaceAll('>', '&gt;')
