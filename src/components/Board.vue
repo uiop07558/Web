dd3dfbf4db1cb737141154e59e4fff4682d53579
Maslov Dmitry <dmitry.maslov.almeza@gmail.com>
2022-06-23 15:00:16 +0300

    Fix скролл в досках

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -60,7 +60,7 @@
       @cancel="showMoveCard = false"
       @changePosition="onChangeCardPosition"
     />
-    <div class="flex items-start overflow-y-auto">
+    <div class="flex items-start overflow-x-auto">
       <template
         v-for="column in storeCards"
         :key="column.UID"
