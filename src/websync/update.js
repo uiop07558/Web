fd4d7006b4a1435d22f1391983e291efa47745d8
Gashilov Dmitry <gashilovdmitry@yandex.ru>
2022-06-24 09:28:11 +0300

    remove task function from update.js

--- src/websync/update.js
+++ src/websync/update.js
@@ -1,5 +1,5 @@
 import * as TYPES from '@/websync/types.js'
-import { task, updateTask } from '@/websync/task.js'
+import { updateTask } from '@/websync/task.js'
 import { getMessage } from '@/websync/task_message.js'
 import { updateCard } from '@/websync/card.js'
 import { updateEmployee } from '@/websync/employee.js'
@@ -18,7 +18,6 @@ export default function processUpdate (obj) {
       break
     case TYPES.TYPE_OBJECT_TASK:
       updateTask(obj)
-      task(obj)
       break
     case TYPES.TYPE_OBJECT_CONTACT:
       break
