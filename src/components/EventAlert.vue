e78b06c090a556797adf9eadf22e96f79bcd2869
Teegroon <teegroon@gmail.com>
2022-06-29 01:36:47 +0300

    Сделан компонент акции для навигатора

---
+++ src/components/EventAlert.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    :class="`${ boxColor } `"
+    class="p-6 max-w-sm mx-auto rounded-xl shadow-lg flex items-start space-x-4"
+  >
+    <Icon
+      :path="DefaultIcon.path"
+      class="flex-none h-7 w-7"
+      :box="DefaultIcon.viewBox"
+      :width="DefaultIcon.width"
+      :height="DefaultIcon.height"
+    />
+    <p class="w-full">
+      {{ messageText }}
+    </p>
+    <Icon
+      :path="CloseIcon.path"
+      class="flex-none h-7 w-7"
+      :box="CloseIcon.viewBox"
+      :width="CloseIcon.width"
+      :height="CloseIcon.height"
+      @click="$emit('close')"
+    />
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import Icon from '@/components/Icon.vue'
+import DefaultIcon from '@/icons/msgs.js'
+import CloseIcon from '@/icons/close.js'
+import { colorsBg } from '@/colors'
+
+const props = defineProps({
+  bgColor: {
+    type: String,
+    default: 'white'
+  },
+  messageText: {
+    type: String,
+    default: ''
+  }
+})
+
+const boxColor = computed(() => colorsBg[props.bgColor])
+
+defineEmits(['close'])
+
+</script>
