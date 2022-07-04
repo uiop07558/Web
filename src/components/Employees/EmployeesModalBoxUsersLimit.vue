8fc3aacb47b6ea1342e35bdb4f308fe1dfdfb9ea
kakeoff <jasonsuper04@gmail.com>
2022-07-04 10:46:45 +0300

    модалка на добавление сотрудников

--- src/components/Employees/EmployeesModalBoxUsersLimit.vue
+++ src/components/Employees/EmployeesModalBoxUsersLimit.vue
@@ -1,16 +1,24 @@
 <template>
   <ModalBox
     title="Добавить сотрудника"
-    ok="OK"
-    @ok="onOk"
     @cancel="onCancel"
   >
-    <div class="text-[#7e7e80] text-[13px] leading-[18px] font-roboto whitespace-pre-line">
-      Превышен лимит сотрудников в организации. Чтобы добавить сотрудника нужно расширить вашу лицензию.
+    <div class="flex flex-col">
+      <div class="text-[#7e7e80] text-[13px] leading-[18px] font-roboto whitespace-pre-line">
+        Чтобы добавить сотрудника, необходимо докупить рабочее место
+      </div>
+      <div class="gap-[4px] flex justify-end mt-4">
+        <a
+          href="https://www.leadertask.ru/alpha"
+          target="_blank"
+          class="focus:ring min-w-[90px] focus:outline-none inline-flex cursor-pointer whitespace-nowrap justify-center items-center duration-150 px-[12px] py-[10px] rounded-md bg-[#ff9123] text-white text-[13px] leading-[15px] font-medium font-roboto"
+        >
+          Купить
+        </a>
+      </div>
     </div>
   </ModalBox>
 </template>
-
 <script>
 import ModalBox from '@/components/modals/ModalBox.vue'
 
@@ -25,6 +33,7 @@ export default {
     },
     onOk () {
       this.$emit('ok')
+      this.$router.push('/home')
     }
   }
 }
