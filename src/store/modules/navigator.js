1afb5d8808086cdba8e7eb501a184b9e9e5accba
Gashilov Dmitry <gashilovdmitry@yandex.ru>
2022-07-11 11:07:29 +0300

    added reglaments into menu array

--- src/store/modules/navigator.js
+++ src/store/modules/navigator.js
@@ -210,22 +210,7 @@ const mutations = {
 
     console.log('navigator ', resp)
 
-    // Push statickly tasks to menu array from state
     state.memu = []
-    // state.menu.push('separator')
-    // state.menu.push([
-    //   {
-    //     label: 'Рабочий стол',
-    //     uid: '2bad1413-a373-4926-8a3c-58677a680714',
-    //     bold: 0,
-    //     icon: desktop.path,
-    //     width: desktop.width,
-    //     height: desktop.height,
-    //     iconBox: desktop.viewBox,
-    //     type: 'uid',
-    //     iconBackgroundClass: 'bg-white-500'
-    //   }
-    // ])
     state.menu.push([
       {
         label: 'Очередь',
@@ -279,6 +264,20 @@ const mutations = {
         height: 30,
         type: 'greed',
         path: 'new_private_projects',
+        iconBackgroundClass: 'bg-amber-500'
+      }
+    ])
+    state.menu.push([
+      {
+        label: 'Регламенты',
+        uid: resp.data.private_projects.uid,
+        bold: false,
+        icon: 'M23.8818 9.48613V8.39669C23.8818 7.38613 23.0651 6.56289 22.0625 6.56289H14.565L14.3401 6.18824C14.2031 5.97134 13.9635 5.83331 13.7092 5.83331H8.15332C7.15073 5.83331 6.33398 6.65655 6.33398 7.66712V21.4995C6.33398 22.5101 7.15073 23.3333 8.15332 23.3333H23.6813C24.6839 23.3333 25.5006 22.5101 25.5006 21.4995V11.3101C25.5055 10.3636 24.7915 9.58472 23.8818 9.48613ZM7.81097 7.66712C7.81097 7.47486 7.96748 7.31712 8.15821 7.31712H13.2983L15.2937 10.6002C15.4258 10.8171 15.6654 10.9551 15.9246 10.9551H23.6911C23.8818 10.9551 24.0383 11.1129 24.0383 11.3051V21.4995C24.0383 21.6918 23.8818 21.8495 23.6911 21.8495H8.15332C7.96259 21.8495 7.80608 21.6918 7.80608 21.4995V7.66712H7.81097ZM22.4048 9.47134H16.3306L15.4649 8.04669H22.0576C22.2483 8.04669 22.4048 8.20444 22.4048 8.39669V9.47134Z',
+        iconBox: '0 0 30 30',
+        width: 30,
+        height: 30,
+        type: 'greed',
+        path: 'reglaments',
         iconBackgroundClass: 'bg-amber-500'
       }
     ])
@@ -356,6 +355,7 @@ const mutations = {
       newEmps.push(dep)
     }
     resp.data.new_emps = newEmps
+
     // Merge common projects and private projects into my own data structure
     // Array of objects where object is { dep: 'Dependency name', items: items }
     const itemsInProjectView = []
@@ -381,6 +381,9 @@ const mutations = {
       items: resp.data.common_boards.items
     })
     resp.data.new_private_boards = newCommonBoards
+
+    // Logic for reglaments
+    resp.data.reglaments = []
 
     state.navigator = resp.data
   },
