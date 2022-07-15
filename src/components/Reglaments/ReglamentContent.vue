ea96cea2f9461550bc4c4ca39c1624bbc9b41e82
olsoncarsen <gashilovdmitry@yandex.ru>
2022-07-15 15:35:34 +0300

    remove doubled uuidv4 method

--- src/components/Reglaments/ReglamentContent.vue
+++ src/components/Reglaments/ReglamentContent.vue
@@ -80,14 +80,6 @@ export default {
         }
       }
     },
-    uuidv4 () {
-      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-        (
-          c ^
-          (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-        ).toString(16)
-      )
-    },
     isEdit () {
       this.currentReglament.content = this.text
       this.$store.dispatch('UPDATE_REGLAMENT_REQUEST', this.currentReglament)
