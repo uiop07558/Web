e6a9bf133637b974016cc7ca37d041229cbeb73b
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2022-07-19 10:05:01 +0300

    Фикс пустого пространства, в случае если нет задач (страница 'Сегодня')

--- src/components/TasksListNew.vue
+++ src/components/TasksListNew.vue
@@ -91,7 +91,8 @@
     <!-- vue3-treeview -->
     <div
       v-if="status == 'success'"
-      class="overflow-y-auto pt-[4px] px-px min-h-[300px] w-full"
+      class="overflow-y-auto pt-[4px] px-px w-full"
+      :class="{'min-h-[300px]': Object.keys(storeTasks).length}"
     >
       <tree
         :nodes="storeTasks"
