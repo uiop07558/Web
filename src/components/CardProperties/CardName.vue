173c7fa31b8fab2fbdf20f7a6421cba1d4078b2f
kakeoff <jasonsuper04@gmail.com>
2022-07-20 14:45:14 +0300

    refactoring CardName.vue

--- src/components/CardProperties/CardName.vue
+++ src/components/CardProperties/CardName.vue
@@ -1,17 +1,19 @@
-<script setup>
-defineEmits(['changeName'])
-const props = defineProps({
-  cardName: String,
-  canEdit: Boolean
-})
+<script>
+export default {
+  props: {
+    cardName: String,
+    canEdit: Boolean
+  },
+  emits: ['changeName']
+}
 </script>
 <template>
   <div
     v-linkified:options="{ className: 'text-blue-600', tagName: 'a' }"
     class="form-control my-[25px] mx-[-15px]"
     style="font-weight: bold; font-size: 18px"
-    :contenteditable="props.canEdit"
+    :contenteditable="canEdit"
     @blur="$emit('changeName', $event)"
-    v-html="props.cardName?.replaceAll('\n','<br/>')"
+    v-html="cardName?.replaceAll('\n','<br/>')"
   />
 </template>
