20b02778dd359704d9ca348db97567ac151eab4d
olsoncarsen <gashilovdmitry@yandex.ru>
2022-07-22 18:20:47 +0300

    фикс ошибок, после фикса конфликтов

--- src/components/Reglaments/ReglamentContent.vue
+++ src/components/Reglaments/ReglamentContent.vue
@@ -35,13 +35,15 @@
     />
     <div
       class="flex font-['Roboto'] text-[#7E7E80] dark:bg-gray-700 dark:text-gray-100 rounded-lg text-[13px] font-medium"
-    <PopMenuItem
-      v-if="!isTesting"
-      class="bg-white mr-1"
-      icon="edit"
-      @click="isEdit"
     >
-      В тесте будут представлены следующие вопросы:
+      <PopMenuItem
+        v-if="!isTesting"
+        class="bg-white mr-1"
+        icon="edit"
+        @click="isEdit"
+      >
+        В тесте будут представлены следующие вопросы:
+      </popmenuitem>
     </div>
   </div>
   <div v-else>
