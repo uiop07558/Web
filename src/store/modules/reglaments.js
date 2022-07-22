b3e18a61ac2fc83a33d77f7ee58423c1e4491362
Bukharin Maxim <jojojopa@icloud.com>
2022-07-22 14:11:48 +0300

    запрос на удаление ответов пользователей/настройка в режиме редактировании

--- src/store/modules/reglaments.js
+++ src/store/modules/reglaments.js
@@ -72,6 +72,17 @@ const actions = {
         })
     })
   },
+  [REGLAMENTS.DELETE_USERS_REGLAMENT_ANSWERS]: ({ commit, dispatch }, uidReglament) => {
+    return new Promise((resolve, reject) => {
+      const url = process.env.VUE_APP_INSPECTOR_API + 'deleteUserReglamentAnswers?uid_reglament=' + uidReglament
+      axios({ url: url, method: 'DELETE' })
+        .then(resp => {
+          resolve(resp)
+        }).catch(err => {
+          reject(err)
+        })
+    })
+  },
   [REGLAMENTS.DELETE_REGLAMENT_REQUEST]: ({ commit, dispatch }, uid) => {
     return new Promise((resolve, reject) => {
       const url = process.env.VUE_APP_INSPECTOR_API + 'reglaments?uid=' + uid
