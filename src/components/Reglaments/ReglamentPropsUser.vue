eaa6def0eae280ac21cefd960e694888abae4c46
Maslov Dmitry <63534992+devmaslove@users.noreply.github.com>
2022-07-28 13:46:33 +0300

    Рефакторинг
    Убрал фото если нет, имя пользователя если нет такого вопросами

--- src/components/Reglaments/ReglamentPropsUser.vue
+++ src/components/Reglaments/ReglamentPropsUser.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="group w-full h-[34px] flex items-center border-[2px] px-2 mb-1 rounded">
     <img
-      :src="employees[userUid]?.fotolink"
+      v-if="userPhoto"
+      :src="userPhoto"
       class="flex-none border border-[#7e7e80] rounded-[4px] w-[20px] h-[20px] mr-[7px]"
     >
-    <span class="grow font-roboto text-[13px] leading-[20px] font-medium text-[#4c4c4d] mr-[7px]">{{ employees[userUid]?.name || employees[userUid]?.email }}</span>
+    <span class="grow font-roboto text-[13px] leading-[20px] font-medium text-[#4c4c4d] mr-[7px]">
+      {{ userName }}
+    </span>
   </div>
 </template>
 <script>
@@ -16,8 +19,14 @@ export default {
     }
   },
   computed: {
-    employees () {
-      return this.$store.state.employees.employees
+    user () {
+      return this.$store.state.employees.employees[this.userUid]
+    },
+    userPhoto () {
+      return this.user?.fotolink ?? ''
+    },
+    userName () {
+      return this.user?.name || this.user?.email || '???'
     }
   }
 }
